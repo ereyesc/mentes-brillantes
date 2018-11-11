b88ad16a3d2fe85aebcca4772f20d370279ddbd8
Reyes Cruz <e.reyes.cruz@accenture.com>
2018-11-11 10:25:47 -0600

    adding code for client side work

---
+++ src/app/app.module.ts
@@ -0,0 +1,71 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+
+import { VgCoreModule } from 'videogular2/core';
+import { VgControlsModule } from 'videogular2/controls';
+
+import { AlertComponent } from './_directives';
+import { AuthGuard } from './_guards';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AlertService, AuthenticationService, UserService } from './_services';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { CoursesComponent } from './courses/courses.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+import { VideoPlaylistComponent } from './video-playlist/video-playlist.component'
+import { CourseComponent } from './course/course.component';
+import { InfoPadresComponent } from './info-padres/info-padres.component';
+import { PlanesComponent } from './planes/planes.component';
+import { NotFoundComponent } from './not-found/not-found.component'
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        routing,
+        VgCoreModule,
+        VgControlsModule
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        CoursesComponent,
+        DashboardComponent,
+        HeaderComponent,
+        FooterComponent,
+        VideoPlayerComponent,
+        VideoPlaylistComponent,
+        CourseComponent,
+        InfoPadresComponent,
+        PlanesComponent,
+        NotFoundComponent],
+    providers: [
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        UserService,
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
